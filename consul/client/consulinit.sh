b09123c211739fcf168f690b86aeb39e2e1a8521
Joseph Gorse <josephgorse@users.noreply.github.com>
2016-08-05 13:28:22 -0400

    Consul config change (#5)
    * Updated consul version to valid release. Start client with script to bind to eth0 instead of 0.0.0.0
    
    * Fixed config file path in consul
    
    * removed underscore from container names

---
+++ consul/client/consulinit.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+client_addr="$(ifconfig eth0 | grep 'inet addr' | awk '{split($2, a, ":"); print a[2]}')"
+
+consul agent -client ${client_addr} -config-dir /etc/consul.d -retry-interval 10s -retry-join consulserver
