3ed9579fa5d8600077a52eb6d7ff678190eeb5a4
James King <james.king@emc.com>
2016-08-10 13:00:28 -0400

    Chowned go directory for user

--- golang/entrypoint.sh
+++ golang/entrypoint.sh
@@ -8,6 +8,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+chown -R $USER_ID /go
 export HOME=/home/user
 
 exec /usr/local/bin/gosu user "$@"
